c55148c2c16f8356d8dd9e9a178eb4e1f32f7c3f
dbrannonhoward <dbrannonhoward@gmail.com>
2020-05-18 20:20:13 -0400

    tutorial 2 completed

--- role.harvester.js
+++ role.harvester.js
@@ -8,7 +8,7 @@ var roleHarvester = {
                 creep.moveTo(sources[0]);
             }
         }
-        else {
+        else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
             if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.spawns['Spawn1']);
             }
